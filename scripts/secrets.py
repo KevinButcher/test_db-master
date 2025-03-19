d51c459407e3cd776de955d530c86d03bfef5f36
KevinButcher <111787718+KevinButcher@users.noreply.github.com>
2025-03-19 11:53:55 -0600

    Delete scripts/secrets.py

--- scripts/secrets.py
+++
@@ -1,8 +0,0 @@
-# secrets.py
-secrets = {
-    'SECRET_KEY': "superSecretkey1234",
-    'DATABASE_USER': "root",
-    'DATABASE_PASSWORD': 'Blackdeath9!',
-    'DATABASE_PORT': 3306,
-    'SECRET_KEY2': 'Blackdeath9'
-}
